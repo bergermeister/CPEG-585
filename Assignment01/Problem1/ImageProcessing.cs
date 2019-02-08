0d9d26610c1ef5e7cb99892a2b52a203e882597f
Edward Eisenberger <eeisenberger3@gmail.com>
2019-02-08 13:20:01 -0500

    Updated ImageProcessing.cs

--- Assignment01/Problem1/ImageProcessing.cs
+++ Assignment01/Problem1/ImageProcessing.cs
@@ -11,12 +11,13 @@ namespace Problem1
          // TODO: Add constructor logic
       }
 
-      public static bool MConvertToGray( Bitmap aoBmp )
+      public static Bitmap MConvertToGray( Bitmap aoBmp )
       {        
-         int   kiI;
-         int   kiJ;
-         int   kiGray;
-         Color koColor;
+         int    kiI;
+         int    kiJ;
+         int    kiGray;
+         Color  koColor;
+         Bitmap koBmp = new Bitmap( aoBmp.Width, aoBmp.Height );
 
          for( kiI = 0; kiI < aoBmp.Width; kiI++ )
          {
@@ -31,11 +32,11 @@ namespace Problem1
                                   ( 0.114 * koColor.B ) );
 
                // Set the pixel to the grayscale value
-               aoBmp.SetPixel( kiI, kiJ, Color.FromArgb( kiGray, kiGray, kiGray ) );
+               koBmp.SetPixel( kiI, kiJ, Color.FromArgb( kiGray, kiGray, kiGray ) );
             }
          }
 
-         return( true );
+         return( koBmp );
       }
 
       public static Bitmap MBrighten( Bitmap aoUse, int aiBrightness )
@@ -86,11 +87,88 @@ namespace Problem1
 
          return( koBright );
       }
-      /*
+      
       public static Bitmap Contrast( Bitmap aoBmp, sbyte acContrast )
       {
+         Bitmap koBmp = new Bitmap( aoBmp.Width, aoBmp.Height );
+         Color  koColor;
+         double kdPixel = 0.0;
+         double kdContrast = ( 100.0 + acContrast ) / 100.0;
+         int[ ] kipC = new int[ 3 ];
+         int    kiI;
+         int    kiY;
+         int    kiX;
 
+         if( acContrast < -100 ) acContrast = -100;
+         if( acContrast >  100 ) acContrast =  100;
+
+         for( kiY = 0; kiY < aoBmp.Width; kiY++ )
+         {
+            for( kiX = 0; kiX < aoBmp.Width; kiX++ )
+            {
+               koColor = aoBmp.GetPixel( kiX, kiY );
+
+               kipC[ 0 ] = Convert.ToInt32( koColor.R );
+               kipC[ 1 ] = Convert.ToInt32( koColor.G );
+               kipC[ 2 ] = Convert.ToInt32( koColor.B );
+
+               for( kiI = 0; kiI < 3; kiI++ )
+               {
+                  kdPixel = kipC[ kiI ] / 255.0;
+                  kdPixel -= 0.5;
+                  kdPixel *= kdContrast;
+                  if( kdPixel < 0 ) kdPixel = 0;
+                  if( kdPixel > 255 ) kdPixel = 255;
+                  kipC[ kiI ] = ( int )kdPixel;
+               }
+
+               koBmp.SetPixel( kiX, kiY, Color.FromArgb( kipC[ 0 ], kipC[ 1 ], kipC[ 2 ] ) );
+            }
+         }
+
+         return( koBmp );
       }
-      */
+      
+      public static Bitmap RecoverBitmapFromArray( double[ ] adpBmp, int aiWidth, int aiHeight )
+      {
+         Bitmap koBmp = new Bitmap( aiWidth, aiHeight );
+         int    kiP = 0;
+         int    kiQ = 0;
+         int    kiI;
+         int    kiVal;
+
+         for( kiI = 0; kiI < adpBmp.Length; kiI++ )
+         {
+            kiVal = ( int )( adpBmp[ kiI ] / ( 1375669f * 900f ) );
+            if( kiVal < 0 )
+            {
+               kiVal = 0;
+            }
+
+            koBmp.SetPixel( kiP, kiQ, Color.FromArgb( kiVal, kiVal, kiVal ) );
+            kiP++;
+            if( kiP == aiWidth )
+            {
+               kiP = 0;
+               kiQ++;
+            }
+         }
+
+         return( koBmp );
+      }
+ 
+      public static void RotateClear( Image aoImg, ref Bitmap aoBmp, double kdRot )
+      {
+         Graphics koDC;
+
+         while( kdRot >  360.0 ){ kdRot -= 360.0; }
+         while( kdRot < -360.0 ){ kdRot += 360.0; }
+
+         aoBmp = new Bitmap( aoImg.Width, aoImg.Height, PixelFormat.Format24bppRgb );
+         koDC = Graphics.FromImage( aoBmp );
+         koDC.Clear( Color.White );
+         koDC.RotateTransform( ( float )kdRot );
+         koDC.DrawImage( aoImg, new Rectangle( 0, 0, aoImg.Width, aoImg.Height ) );
+      }
    }
 }

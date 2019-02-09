9e90874e2bb01da010e74508a7eae41cfa391599
Edward Eisenberger <eeisenberger3@gmail.com>
2019-02-09 11:31:45 -0500

    Implemented Problem 1

--- Assignment01/Problem1/Form1.cs
+++ Assignment01/Problem1/Form1.cs
@@ -5,16 +5,346 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
 
 namespace Problem1
 {
-   public partial class Form1 : Form
-   {
-      public Form1()
-      {
-         InitializeComponent();
-      }
-   }
+    public partial class Form1 : Form
+    {
+        private Point startPoint, endPoint, startRPoint;
+        bool modeRectangle = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "jpeg files (*.jpg)|*.jpg|(*.gif)|gif||";
+            if (DialogResult.OK == dialog.ShowDialog())
+            {
+                this.picOrigImage.Image = new Bitmap(dialog.FileName);
+                FileInfo finfo = new FileInfo(dialog.FileName);
+                lblImageUnderTest.Text = "Image Under Test\n " +
+                    finfo.Name;
+                txtWidthOrig.Text = picOrigImage.Image.Width.ToString();
+                txtHeightOrig.Text = picOrigImage.Image.Height.ToString();
+            }
+        }
+
+        private void btnConvertToGray_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Bitmap copy = ImageProcessing.MConvertToGray((Bitmap)this.picOrigImage.Image);
+                picProcImage.Image = null;
+                picProcImage.Image = copy;
+                txtWidthProc.Text = picProcImage.Image.Width.ToString();
+                txtHeightProc.Text = picProcImage.Image.Height.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnRotateFillImage_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FormRotate dlg = new FormRotate();
+                dlg.RotationAngle = 0;
+                Image origimg = picOrigImage.Image;
+                Bitmap bmp = new Bitmap(origimg);
+                if (DialogResult.OK == dlg.ShowDialog())
+                {
+                    ImageProcessing.MRotateFill(origimg, ref bmp, dlg.RotationAngle);
+                    picProcImage.Image = null;
+                    picProcImage.Image = bmp;
+                    txtWidthProc.Text = picProcImage.Image.Width.ToString();
+                    txtHeightProc.Text = picProcImage.Image.Height.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnCropAroundMouseClick_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Image img = this.picOrigImage.Image;
+                Bitmap bmp = new Bitmap(img);
+                Point p1 = new Point(startPoint.X - int.Parse(txtCropAroundW.Text) / 2,
+                    startPoint.Y - int.Parse(txtCropAroundH.Text) / 2);
+                Rectangle rect = new Rectangle(p1, new Size(int.Parse(txtCropAroundW.Text),
+                    int.Parse(txtCropAroundH.Text)));
+
+                bmp = new Bitmap(img);
+                ImageProcessing.MCropImage(img, ref bmp, rect);
+                picProcImage.Image = bmp;
+                txtWidthProc.Text = picProcImage.Image.Width.ToString();
+                txtHeightProc.Text = picProcImage.Image.Height.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void picOrigImage_MouseDown(object sender, MouseEventArgs e)
+        {
+            modeRectangle = true;
+            startPoint = new Point(e.X, e.Y);
+        }
+
+        private void picOrigImage_MouseUp(object sender, MouseEventArgs e)
+        {
+            endPoint = new Point(e.X, e.Y);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtCropAroundW.Text = "14";
+            txtCropAroundH.Text = "14";  // 14 x14 window to crop around
+        }
+
+        private void btnBrightenImage_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FormBrightness dlg = new FormBrightness();
+                dlg.Brightness = 30;  // default brightness of 30
+
+                if (DialogResult.OK == dlg.ShowDialog())
+                {
+                    Bitmap copy = new Bitmap((Bitmap)this.picOrigImage.Image);
+                    copy = ImageProcessing.MBrighten(copy, dlg.Brightness);
+
+                    picProcImage.Image = null;
+                    picProcImage.Image = copy;
+                    txtWidthProc.Text = picProcImage.Image.Width.ToString();
+                    txtHeightProc.Text = picProcImage.Image.Height.ToString();
+                 }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnChangeContrast_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FormContrast dlg = new FormContrast();
+                dlg.Contrast = 0;
+
+                if (DialogResult.OK == dlg.ShowDialog())
+                {
+                    Bitmap copy = new Bitmap((Bitmap)this.picOrigImage.Image);
+                    copy = ImageProcessing.MContrast(copy, (sbyte)dlg.Contrast);
+
+                    picProcImage.Image = null;
+                    picProcImage.Image = copy;
+                    txtWidthProc.Text = picProcImage.Image.Width.ToString();
+                    txtHeightProc.Text = picProcImage.Image.Height.ToString();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSaveProcessedImage_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "jpeg files (*.jpg)|*.jpg";
+            if (DialogResult.OK == dlg.ShowDialog())
+                this.picProcImage.Image.Save(dlg.FileName, ImageFormat.Jpeg);
+        }
+
+        private void btnResizeImage_Click(object sender, EventArgs e)
+        {
+            if (modeRectangle == true)
+            {
+                modeRectangle = false;
+                Rectangle rect = new Rectangle(new Point(0, 0), new Size(endPoint.X, endPoint.Y));
+                Image img = this.picOrigImage.Image;
+                Bitmap bmp = new Bitmap(img);
+                ImageProcessing.MResizeImage(img, ref bmp, rect);
+                picProcImage.Image = bmp;
+                txtWidthProc.Text = picProcImage.Image.Width.ToString();
+                txtHeightProc.Text = picProcImage.Image.Height.ToString();
+            }
+            else
+                MessageBox.Show("you must click mouse to select bottom of rectangle");
+        }
+
+        private void btnResizeProportional_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                modeRectangle = false;
+                int Width = int.Parse(txtNewSizeW.Text);
+                int Height = int.Parse(txtNewSizeH.Text);
+                Rectangle rect = new Rectangle(new Point(0, 0), new Size(Width, Height));
+                Image img = this.picOrigImage.Image;
+                Bitmap bmp = new Bitmap(img);
+                ImageProcessing.MResizeImageProportional(img, ref bmp, rect);
+                picProcImage.Image = bmp;
+                txtWidthProc.Text = picProcImage.Image.Width.ToString();
+                txtHeightProc.Text = picProcImage.Image.Height.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnCropAroundDragAndReleaseMouse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (modeRectangle == true)
+                {
+                    modeRectangle = false;
+                    Rectangle rect = new Rectangle(startPoint, new Size(endPoint.X - startPoint.X, endPoint.Y - startPoint.Y));
+                    Image img = this.picOrigImage.Image;
+                    Bitmap bmp = new Bitmap(img);
+                    ImageProcessing.MCropImage(img, ref bmp, rect);
+                    picProcImage.Image = bmp;
+                    txtWidthProc.Text = picProcImage.Image.Width.ToString();
+                    txtHeightProc.Text = picProcImage.Image.Height.ToString();
+                }
+                else
+                    MessageBox.Show("you must click mouse to select bottom of rectangle");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnDraRectangle_Click(object sender, EventArgs e)
+        {
+            if (modeRectangle == true)
+            {
+                modeRectangle = false;
+                Rectangle rect = new Rectangle(startPoint, new Size(endPoint.X - startPoint.X, endPoint.Y - startPoint.Y));
+                Image img = this.picOrigImage.Image;
+                Bitmap bmp = new Bitmap(img);
+
+                ImageProcessing.MDrawRectangle(img, ref bmp, rect);
+                picProcImage.Image = bmp;
+                txtWidthProc.Text = picProcImage.Image.Width.ToString();
+                txtHeightProc.Text = picProcImage.Image.Height.ToString();
+            }
+            else
+                MessageBox.Show("you must drag mouse to select rectangle");
+        }
+
+        private void btnRotateClear_Click(object sender, EventArgs e)
+        {
+            FormRotate frmRotate = new FormRotate();
+            frmRotate.RotationAngle = 0;
+            Image origimg = picOrigImage.Image;
+            Bitmap bmp = new Bitmap(origimg);
+            if (DialogResult.OK == frmRotate.ShowDialog())
+            {
+                ImageProcessing.MRotateClear(origimg, ref bmp, frmRotate.RotationAngle);
+                picProcImage.Image = null;
+                picProcImage.Image = bmp;
+                txtWidthProc.Text = picProcImage.Image.Width.ToString();
+                txtHeightProc.Text = picProcImage.Image.Height.ToString();
+            }
+
+        }
+
+        private void btnDrawX_Click(object sender, EventArgs e)
+        {
+            Image origimg = picOrigImage.Image;
+            Bitmap bmp = new Bitmap(origimg);
+            ImageProcessing.MDrawX(origimg, ref bmp, startPoint);
+            picProcImage.Image = null;
+            picProcImage.Image = bmp;
+        }
+
+        private void btnRotateByPoints_Click(object sender, EventArgs e)
+        {
+            Image origimg = picOrigImage.Image;
+            Bitmap bmp = new Bitmap(origimg);
+            if (modeRectangle == true)
+            {
+                ImageProcessing.MRotateByPoints(origimg, ref bmp, startPoint, endPoint);
+                picProcImage.Image = null;
+                picProcImage.Image = bmp;
+                txtWidthProc.Text = picProcImage.Image.Width.ToString();
+                txtHeightProc.Text = picProcImage.Image.Height.ToString();
+            }
+            else
+                MessageBox.Show("you must click mouse to select bottom of rectangle");
+        }
+
+        private void btnRotateFill_Click(object sender, EventArgs e)
+        {
+            FormRotate dlg = new FormRotate();
+            dlg.RotationAngle = 0;
+            Image origimg = picOrigImage.Image;
+            Bitmap bmp = new Bitmap(origimg);
+            if (DialogResult.OK == dlg.ShowDialog())
+            {
+                ImageProcessing.MRotateFill(origimg, ref bmp, dlg.RotationAngle);
+                picProcImage.Image = null;
+                picProcImage.Image = bmp;
+                txtWidthProc.Text = picProcImage.Image.Width.ToString();
+                txtHeightProc.Text = picProcImage.Image.Height.ToString();
+            }
+        }
+
+        private void btnShiftHorizontal_Click(object sender, EventArgs e)
+        {
+            FormShift dlg = new FormShift();
+            dlg.ShiftAmount = 0;
+            Image origimg = picOrigImage.Image;
+            Bitmap bmp = new Bitmap(origimg);
+            if (DialogResult.OK == dlg.ShowDialog())
+            {
+                if (ImageProcessing.MShiftImageHorizontal(origimg, ref bmp, dlg.ShiftAmount))
+                {
+                    picProcImage.Image = null;
+                    picProcImage.Image = bmp;
+                    txtWidthProc.Text = picProcImage.Image.Width.ToString();
+                    txtHeightProc.Text = picProcImage.Image.Height.ToString();
+                 }
+            }
+        }
+
+        private void btnShiftVertical_Click(object sender, EventArgs e)
+        {
+            FormShift dlg = new FormShift();
+            dlg.ShiftAmount = 0;
+            Image origimg = picOrigImage.Image;
+            Bitmap bmp = new Bitmap(origimg);
+            if (DialogResult.OK == dlg.ShowDialog())
+            {
+                if (ImageProcessing.MShiftImageVertical(origimg, ref bmp, dlg.ShiftAmount))
+                {
+                    picProcImage.Image = null;
+                    picProcImage.Image = bmp;
+                    txtWidthProc.Text = picProcImage.Image.Width.ToString();
+                    txtHeightProc.Text = picProcImage.Image.Height.ToString();
+                 }
+            }
+        }
+    }
 }
--- Assignment01/Problem1/ImageProcessing.cs
+++ Assignment01/Problem1/ImageProcessing.cs
@@ -88,7 +88,7 @@ namespace Problem1
          return( koBright );
       }
       
-      public static Bitmap Contrast( Bitmap aoBmp, sbyte acContrast )
+      public static Bitmap MContrast( Bitmap aoBmp, sbyte acContrast )
       {
          Bitmap koBmp = new Bitmap( aoBmp.Width, aoBmp.Height );
          Color  koColor;
@@ -129,7 +129,7 @@ namespace Problem1
          return( koBmp );
       }
       
-      public static Bitmap RecoverBitmapFromArray( double[ ] adpBmp, int aiWidth, int aiHeight )
+      public static Bitmap MRecoverBitmapFromArray( double[ ] adpBmp, int aiWidth, int aiHeight )
       {
          Bitmap koBmp = new Bitmap( aiWidth, aiHeight );
          int    kiP = 0;
@@ -157,18 +157,238 @@ namespace Problem1
          return( koBmp );
       }
  
-      public static void RotateClear( Image aoImg, ref Bitmap aoBmp, double kdRot )
+      public static void MRotateClear( Image aoImg, ref Bitmap aoBmp, double adRot )
       {
          Graphics koDC;
 
-         while( kdRot >  360.0 ){ kdRot -= 360.0; }
-         while( kdRot < -360.0 ){ kdRot += 360.0; }
+         while( adRot >  360.0 ){ adRot -= 360.0; }
+         while( adRot < -360.0 ){ adRot += 360.0; }
 
          aoBmp = new Bitmap( aoImg.Width, aoImg.Height, PixelFormat.Format24bppRgb );
          koDC = Graphics.FromImage( aoBmp );
          koDC.Clear( Color.White );
-         koDC.RotateTransform( ( float )kdRot );
+         koDC.RotateTransform( ( float )adRot);
          koDC.DrawImage( aoImg, new Rectangle( 0, 0, aoImg.Width, aoImg.Height ) );
+      }
+
+      public static void MRotateByPoints( Image aoImg, ref Bitmap aorBmp, Point aoP1, Point aoP2 )
+      {
+         Point  koMid;  // Mid point between P1 and P2
+         Point  koNew;  // New mid point
+         double kdRad;
+         double kdRot;
+
+         if( aoP1.X != aoP2.X )
+         {
+            koMid = new Point();
+            koMid.X = ( int )( ( aoP1.X + aoP2.X ) / 2.0 );
+            koMid.Y = ( int )( ( aoP1.Y + aoP2.Y ) / 2.0 );
+            kdRad = Math.Atan2( -( aoP2.Y - aoP1.Y ), ( aoP2.X - aoP1.X ) );
+            koNew = new Point();
+            koNew.X = ( int )( ( koMid.X * Math.Cos( kdRad ) ) - ( koMid.Y * Math.Sin( kdRad ) ) );
+            koNew.Y = ( int )( ( koMid.Y * Math.Cos( kdRad ) ) + ( koMid.X * Math.Sin( kdRad ) ) );
+            kdRot = kdRad * 180 / 3.141516;
+            ImageProcessing.MRotateFill2( aoImg, ref aorBmp, kdRot, koMid, koNew );
+         }
+      }
+
+      public static void MRotateFill( Image aoImg, ref Bitmap aorBmp, double adRot )
+      {
+         Graphics koDC;
+         double   kdDegrees = adRot * 3.141516 / 180;
+         int      kiShift   = ( int )( ( aorBmp.Height / 2 ) * Math.Tan( kdDegrees ) );
+
+         while( adRot >  360.0 ){ adRot -= 360.0; }
+         while( adRot < -360.0 ){ adRot += 360.0; }
+
+         aorBmp = new Bitmap( aoImg.Width, aoImg.Height, PixelFormat.Format24bppRgb );
+         koDC = Graphics.FromImage( aorBmp );
+         koDC.DrawImage( aoImg, new Rectangle( 0, 0, aoImg.Width, aoImg.Height ) );
+         koDC.RotateTransform( ( float) adRot );
+         if( kdDegrees > 0 )
+         {
+            koDC.DrawImage( aoImg, new Rectangle( kiShift, -kiShift, aoImg.Width, aoImg.Height ) );
+         }
+			else
+         { 
+			   koDC.DrawImage( aoImg, new Rectangle( kiShift, kiShift / 2, aoImg.Width, aoImg.Height ) );
+         }
+      }
+
+      public static void MRotateFill2( Image aoImg, ref Bitmap aorBmp, double adRot, Point aoOld, Point aoNew )
+      {
+         Graphics koDC;
+         double   kdDeg;
+         int      kiShift;
+
+         while (adRot > 360.0) { adRot -= 360.0; }
+         while (adRot < -360.0) { adRot += 360.0; }
+
+         aorBmp = new Bitmap( aoImg.Width, aoImg.Height, PixelFormat.Format24bppRgb );
+         koDC   = Graphics.FromImage( aorBmp );
+         koDC.DrawImage( aoImg, new Rectangle( 0, 0, aoImg.Width, aoImg.Height ) );
+         koDC.RotateTransform( ( float )adRot );
+         kdDeg = adRot * 3.141516 / 180;
+         kiShift = ( int )( aorBmp.Height / 2 * Math.Tan( kdDeg ) );
+         if( kdDeg > 0 )
+         { 
+            koDC.DrawImage( aoImg, new Rectangle( 0, 0, aoImg.Width, aoImg.Height ) );
+         }
+         else
+         { 
+            koDC.DrawImage( aoImg, new Rectangle( new Point( 0, 0 ), new Size( aoImg.Width, aoImg.Height ) ) );
+         }
+      }
+
+      public static void MDrawRectangle( Image aoImg, ref Bitmap aorBmp, Rectangle aoRec )
+      {
+         Graphics koDC;
+
+         aorBmp = new Bitmap( aoImg.Width, aoImg.Height, PixelFormat.Format24bppRgb );
+         koDC   = Graphics.FromImage( aorBmp );
+         koDC.DrawImage( aoImg, new Rectangle( 0, 0, aoImg.Width, aoImg.Height ) );
+         koDC.DrawRectangle( new Pen( Color.Red ), aoRec );
+      }
+
+      public static void MDrawX( Image aoImg, ref Bitmap aorBmp, Point aoPt )
+      {
+         Graphics koDC;
+         Brush    koBr;
+         Point    koP1, koP2, koP3, koP4;
+
+         aorBmp = new Bitmap( aoImg.Width, aoImg.Height, PixelFormat.Format24bppRgb );
+         koDC   = Graphics.FromImage( aorBmp );
+         koDC.DrawImage( aoImg, new Rectangle( 0, 0, aoImg.Width, aoImg.Height ) );
+         koBr = new SolidBrush( Color.Red );
+         koP1 = new Point( aoPt.X - 3, aoPt.Y - 3 );
+         koP2 = new Point( aoPt.X + 3, aoPt.Y + 3 );
+         koDC.DrawLine( new Pen( Color.Blue ), koP1, koP2 );
+         koP3 = new Point( aoPt.X + 3, aoPt.Y - 3 );
+         koP4 = new Point( aoPt.X - 3, aoPt.Y + 3 );
+         koDC.DrawLine( new Pen( Color.Blue ), koP3, koP4 );
+      }
+
+      public static void Draw2X(Image aoImg, ref Bitmap aorBmp, Point aoPt1, Point aoPt2)
+      {
+         Graphics koDC;
+         Brush    koBr;
+         Point    koP1a, koP1b, koP2a, koP2b, koP3a, koP3b, koP4a, koP4b;
+
+         aorBmp = new Bitmap( aoImg.Width, aoImg.Height, PixelFormat.Format24bppRgb );
+         koDC   = Graphics.FromImage( aorBmp );
+         koDC.DrawImage( aoImg, new Rectangle( 0, 0, aoImg.Width, aoImg.Height ) );
+         koBr = new SolidBrush( Color.Red );
+         koP1a = new Point( aoPt1.X - 3, aoPt1.Y - 3 );
+         koP2a = new Point( aoPt1.X + 3, aoPt1.Y + 3 );
+         koDC.DrawLine( new Pen( Color.Red ), koP1a, koP2a );
+         koP3a = new Point( aoPt1.X + 3, aoPt1.Y - 3 );
+         koP4a = new Point( aoPt1.X - 3, aoPt1.Y + 3 );
+         koDC.DrawLine(new Pen(Color.Red), koP3a, koP4a);
+
+         koP1b = new Point( aoPt2.X - 3, aoPt2.Y - 3 );
+         koP2b = new Point( aoPt2.X + 3, aoPt2.Y + 3 );
+         koDC.DrawLine( new Pen( Color.Red ), koP1b, koP2b );
+         koP3b = new Point( aoPt2.X + 3, aoPt2.Y - 3 );
+         koP4b = new Point( aoPt2.X - 3, aoPt2.Y + 3 );
+         koDC.DrawLine( new Pen( Color.Red ), koP3b, koP4b );
+      }
+
+      public static void MDraw2Xbold( Image aoImg, ref Bitmap aorBmp, Point aoPt1, Point aoPt2 )
+      {
+         Graphics koDC;
+         Brush    koBr;
+         Point    koP1a, koP1b, koP2a, koP2b, koP3a, koP3b, koP4a, koP4b;
+
+         aorBmp = new Bitmap( aoImg.Width, aoImg.Height, PixelFormat.Format24bppRgb );
+         koDC   = Graphics.FromImage( aorBmp );
+         koDC.DrawImage( aoImg, new Rectangle( 0, 0, aoImg.Width, aoImg.Height ) );
+         koBr = new SolidBrush( Color.Red );
+         koP1a = new Point( aoPt1.X - 6, aoPt1.Y - 6 );
+         koP2a = new Point( aoPt1.X + 6, aoPt1.Y + 6 );
+         koDC.DrawLine(new Pen(Color.Red, 2), koP1a, koP2a );
+         koP3a = new Point( aoPt1.X + 6, aoPt1.Y - 6 );
+         koP4a = new Point( aoPt1.X - 6, aoPt1.Y + 6 );
+         koDC.DrawLine( new Pen( Color.Red, 2 ), koP3a, koP4a );
+
+         koP1b = new Point( aoPt2.X - 6, aoPt2.Y - 6 );
+         koP2b = new Point( aoPt2.X + 6, aoPt2.Y + 6 );
+         koDC.DrawLine( new Pen( Color.Red, 2 ), koP1b, koP2b );
+         koP3b = new Point( aoPt2.X + 6, aoPt2.Y - 6 );
+         koP4b = new Point( aoPt2.X - 6, aoPt2.Y + 6 );
+         koDC.DrawLine( new Pen( Color.Red, 2 ), koP3b, koP4b );
+      }
+
+      public static void MResizeImage( Image aoImg, ref Bitmap aorBmp, Rectangle aoRect )
+      {
+         Graphics koDC;
+         aorBmp = new Bitmap( aoRect.Width, aoRect.Height, PixelFormat.Format24bppRgb );
+         koDC = Graphics.FromImage( aorBmp );
+         koDC.DrawImage( aoImg, aoRect);
+      }
+
+      public static void MResizeImageProportional( Image aoImg, ref Bitmap aorBmp, Rectangle aoRect )
+      {
+         Graphics  koDC;
+         Rectangle koRect = new Rectangle( aoRect.X, aoRect.Y, aoRect.Width, aoImg.Height * aoRect.Width / aoImg.Width );
+
+         aorBmp = new Bitmap( koRect.Width, koRect.Height, PixelFormat.Format24bppRgb );
+         koDC   = Graphics.FromImage( aorBmp );
+         koDC.DrawImage( aoImg, koRect );
+      }
+
+      public static void MCropImage( Image aoImg, ref Bitmap aorBmp, Rectangle aoRect )
+      {
+         Bitmap    koCrop = new Bitmap( aoRect.Width, aoRect.Height, aoImg.PixelFormat );
+         Graphics  koDC   = Graphics.FromImage( koCrop );
+         Rectangle koDest = new Rectangle( 0, 0, aoRect.Width, aoRect.Height );
+
+         koDC.DrawImage( aorBmp, koDest, aoRect.X, aoRect.Y, aoRect.Width, aoRect.Height, GraphicsUnit.Pixel );
+
+         aorBmp = koCrop;
+      }
+
+      public static bool MShiftImageHorizontal( Image aoImg, ref Bitmap aorBmp, int aiShiftAmt )
+      {
+         bool     kbStatus;
+         Graphics koDC;
+
+         if( aiShiftAmt > aoImg.Width )
+         {
+            kbStatus = false;
+         }
+         else
+         { 
+            aorBmp = new Bitmap( aoImg.Width, aoImg.Height, PixelFormat.Format24bppRgb );
+            koDC   = Graphics.FromImage( aorBmp );
+            koDC.Clear( Color.Black );
+            koDC.TranslateTransform( aiShiftAmt, 0 );
+            koDC.DrawImage( aoImg, new Rectangle( 0, 0, aoImg.Width, aoImg.Height ) );
+            kbStatus = true;
+         }
+         return( kbStatus );
+      }
+
+      public static bool MShiftImageVertical( Image aoImg, ref Bitmap aorBmp, int aiShiftAmt )
+      {
+         bool     kbStatus;
+         Graphics koDC;
+
+         if( aiShiftAmt > aoImg.Width )
+         {
+            kbStatus = false;
+         }
+         else
+         { 
+            //aorBmp = new Bitmap( aoImg.Width, aoImg.Height + Math.Abs( aiShiftAmt ), PixelFormat.Format24bppRgb );
+            aorBmp = new Bitmap( aoImg.Width, aoImg.Height, PixelFormat.Format24bppRgb );
+            koDC   = Graphics.FromImage( aorBmp );
+            koDC.Clear( Color.Black );
+            koDC.TranslateTransform( 0, aiShiftAmt );
+            koDC.DrawImage( aoImg, new Rectangle( 0, 0, aoImg.Width, aoImg.Height ) );
+            kbStatus = true;
+         }
+         
+         return( kbStatus );
       }
    }
 }

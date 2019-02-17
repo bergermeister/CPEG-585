8840b4643017028e2d59899714fd10864344f890
Edward Eisenberger <eeisenberger3@gmail.com>
2019-02-17 13:07:59 -0500

    Updated Documentation and added example outputs

--- Assignment02/Problem1/TcKernelGaussianDiff.cs
+++ Assignment02/Problem1/TcKernelGaussianDiff.cs
@@ -5,18 +5,23 @@
       public TcKernelGaussianDiff( int aiSize, double adSig1, double adSig2 ) : 
          base( aiSize, ( adSig1 < adSig2 ) ? adSig1 : adSig2 )
       {
+         double[ ][ ] kdpN;
          int kiRow, kiCol;
          int kiCenter = ( this.vdpM.Length - 1 ) / 2;
          int kiOffY, kiOffX;
          int kiScale  = this.vdpM.Length * this.vdpM.Length;
 
+         kdpN = new double[ this.vdpM.Length ][ ];
+
          kiOffY = -kiCenter;
          for( kiRow = 0; kiRow < this.vdpM.Length; kiRow++ )
          {
+            kdpN[ kiRow ] = new double[ this.vdpM.Length ];
             kiOffX = -kiCenter;
             for( kiCol = 0; kiCol < this.vdpM.Length; kiCol++ )
             {
-               this.vdpM[ kiRow ][ kiCol ] -= this.mGaussian( kiOffX, kiOffY, ( adSig1 > adSig2 ) ? adSig1 : adSig2 );
+               kdpN[ kiRow ][ kiCol ]       = this.mGaussian( kiOffX, kiOffY, ( adSig1 > adSig2 ) ? adSig1 : adSig2 );
+               this.vdpM[ kiRow ][ kiCol ] -= kdpN[ kiRow ][ kiCol ];
                kiOffX++;               
             }
             kiOffY++;

d640052e922df13ef6c66667dbcab35ea7eb82cc
Edward Eisenberger <eeisenberger3@gmail.com>
2019-03-09 20:48:50 -0500

    Updated RANSAC algorithm

--- Assignment06/OutlierRemoval/Form1.cs
+++ Assignment06/OutlierRemoval/Form1.cs
@@ -12,6 +12,7 @@ namespace OutlierRemoval
 {
    public partial class Form1 : Form
    {
+      private Random        voRand   = new Random( ( int )DateTime.Now.ToBinary( ) );
       private List< Point > voShape1 = new List< Point >( );
       private List< Point > voShape2 = new List< Point >( );
 
@@ -83,6 +84,7 @@ namespace OutlierRemoval
          Pen pBlue = new Pen(Brushes.Blue, 1);             
          Pen pRed = new Pen(Brushes.Red, 1);             
          Graphics g = this.panel2.CreateGraphics();             
+         g.Clear( this.BackColor );
          this.mDisplayShape( this.voShape1, pBlue, g );             
          this.mDisplayShape( Shape2T, pRed, g ); 
       }
@@ -132,55 +134,136 @@ namespace OutlierRemoval
          List<Point> Shape2T = koTMin.MApply( koShape2 );             
          Pen pBlue = new Pen(Brushes.Blue, 1);             
          Pen pRed = new Pen(Brushes.Red, 1);             
-         Graphics g = this.panel3.CreateGraphics();             
+         Graphics g = this.panel3.CreateGraphics();      
+         g.Clear( this.BackColor );
          this.mDisplayShape( koShape1, pBlue, g );             
          this.mDisplayShape( Shape2T, pRed, g ); 
       }
 
       private void vnBtnORRansac_Click(object sender, EventArgs e)
       {
-         /*
-         input:     data - a set of observations     
-            model - a model that can be fitted to data      
-            n - the minimum number of data required to fit the model     
-            k - the number of iterations performed by the algorithm     
-            t - a threshold value for determining when a datum fits a model     
-            d - the number of close data values required to assert that a model fits well to data 
-         output:     
-            best_model - model parameters which best fit the data (or nil if no good model is found)     
-            best_consensus_set - data point from which this model has been estimated     
-            best_error - the error of this model relative to the data  
- 
-         iterations := 0 
-         best_model := nil 
-         best_consensus_set := nil 
-         best_error := infinity 
-            
-         while iterations < k      
-            maybe_inliers := n randomly selected values from data     
-            maybe_model := model parameters fitted to maybe_inliers     
-            consensus_set := maybe_inliers 
-            
-            for every point in data not in maybe_inliers          
-               if point fits maybe_model with an error smaller than t            
-                  add point to consensus_set          
-                  
-            if the number of elements in consensus_set is > d          
-               (this implies that we may have found a good model,         
-               now test how good it is)         
-               better_model := model parameters fitted to all points in consensus_set         
-               this_error := a measure of how well better_model fits these points         
-               if this_error < best_error             
-                  (we have found a model which is better than any of the previous ones,             
-                   keep it until a better one is found)             
-                  best_model := better_model             
-                  best_consensus_set := consensus_set             
-                  best_error := this_error           
-            
-            increment iterations 
- 
-         return best_model, best_consensus_set, best_error 
-         */
+         Transformation koT;
+         SortedSet< int > koIndices;
+         List< Point >  koImg1;
+         List< Point >  koImg2;
+         double         kdError;
+
+         this.mRANSAC( this.voShape2, this.voShape1, this.voShape1.Count - 1, 10000, 100, this.voShape1.Count - 2, out koT, out koIndices, out kdError );
+
+         koImg1 = this.mGetPoints( this.voShape1, koIndices );
+         koImg2 = this.mGetPoints( this.voShape2, koIndices );
+
+         MessageBox.Show("Cost = " + kdError.ToString( ) );
+
+         List<Point> Shape2T = koT.MApply( koImg2 );             
+         Pen pBlue = new Pen(Brushes.Blue, 1);             
+         Pen pRed = new Pen(Brushes.Red, 1);             
+         Graphics g = this.panel3.CreateGraphics();             
+         g.Clear( this.BackColor );
+         this.mDisplayShape( koImg1, pBlue, g );             
+         this.mDisplayShape( Shape2T, pRed, g ); 
+      }
+
+      /**
+       * @param aoData     A set of observations
+       * @param aoModel    A model that can be fitted to data
+       * @param aiN        The minimum number of data required to fit the model
+       * @param aiK        The number of iterations performed by the algorithm
+       * @param adT        A threshold value for determining when a datum fits a model
+       * @param aiD        The number of close data values required to assert that a model fits well to data 
+       */ 
+      private bool mRANSAC( List< Point > aoData, List< Point > aoModel, int aiN, int aiK, double adT, int aiD, 
+                            out Transformation aoBestModel, out SortedSet< int > aoBestConsensusSet, out double adBestError )
+      {
+         bool           kbSuccess    = false;
+         int            kiIterations = 0;
+         double         kdThisError;
+         SortedSet< int > koMaybeInliers;
+         SortedSet< int > koConsensusSet;
+         List< Point >  koBase;
+         List< Point >  koInput;
+         Transformation koMaybeModel;
+         Transformation koBetterModel;
+
+         aoBestModel        = null;
+         aoBestConsensusSet = null;
+         adBestError        = Double.MaxValue;
+
+         while( kiIterations < aiK )
+         { 
+            koMaybeInliers = this.mGetRandom( aiN, aoData.Count );
+            koBase         = this.mGetPoints( aoModel, koMaybeInliers );
+            koInput        = this.mGetPoints( aoData,  koMaybeInliers );
+            koMaybeModel   = ICPTransformation.ComputeTransformation( koBase, koInput );
+            koConsensusSet = new SortedSet< int >( koMaybeInliers );
+
+            // For every point in data 
+            //for( int kiIdx = 0; kiIdx < aoData.Count; kiIdx++ )
+            //{
+            //   // not in maybe_inliers
+            //   if( !koMaybeInliers.Contains( kiIdx ) )
+            //   {
+            //      koBase.Insert( kiIdx, aoModel[ kiIdx ] );
+            //      koInput.Insert( kiIdx, aoData[ kiIdx ] );
+            //      Transformation koT = ICPTransformation.ComputeTransformation( koBase, koInput );
+            //      if( ICPTransformation.ComputeCost( koBase, koInput, koT ) < adT )
+            //      {
+            //         koConsensusSet.Add( kiIdx );
+            //      }      
+            //   }
+            //}
+
+            // if the number of elements in consensus_set is > d
+            if( koConsensusSet.Count > aiD )
+            { 
+               // This implies that we may have found a good model, now test how good it is
+               koBase        = this.mGetPoints( aoModel, koConsensusSet );
+               koInput       = this.mGetPoints( aoData,  koConsensusSet );
+               koBetterModel = ICPTransformation.ComputeTransformation( koBase, koInput );                  
+               kdThisError   = ICPTransformation.ComputeCost( koBase, koInput, koBetterModel );
+               if( kdThisError < adBestError )
+               { 
+                  // We have found a model which is better than any of the previous ones, keep it until a better one is found
+                  aoBestModel        = koBetterModel;
+                  aoBestConsensusSet = koConsensusSet;
+                  adBestError        = kdThisError;
+                  kbSuccess = true;
+               }
+            }
+
+            kiIterations++; 
+         }
+
+         return( kbSuccess );
+      }
+
+      private SortedSet< int > mGetRandom( int aiCount, int aiMax )
+      {
+         SortedSet< int > koIndices = new SortedSet< int >( );
+         int           kiIdx;
+
+         while( koIndices.Count < aiCount )
+         {
+            kiIdx = this.voRand.Next( aiMax );
+            if( !koIndices.Contains( kiIdx ) )
+            { 
+               koIndices.Add( kiIdx );
+            }
+         }
+
+         return( koIndices );
+      }
+
+      private List< Point > mGetPoints( List< Point > aoPoints, SortedSet< int > aoIndices )
+      {
+         List< Point > koPoints = new List< Point >( aoIndices.Count );
+
+         foreach( int aiIdx in aoIndices )
+         {
+            koPoints.Add( aoPoints[ aiIdx ] );
+         }
+
+         return( koPoints );
       }
    }
 }
